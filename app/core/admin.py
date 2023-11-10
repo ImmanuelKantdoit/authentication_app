85b6cf10d733f14797a6de6667e4d1ef9b6709bf
Miguel Emmanuel Fernandez <m.fernandez.477180@umindanao.edu.ph>
2023-11-10 10:52:27 +0800

    End points added and code polished

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,49 @@
+"""
+Django admin customization
+"""
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users"""
+    ordering = ['id']
+    list_display = ['id', 'email', 'firstname', 'lastname']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('firstname', 'lastname',)}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login', 'id']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'id',
+                'email',
+                'password1',
+                'password2',
+                'firstname',
+                'lastname',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
